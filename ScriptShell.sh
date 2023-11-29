6377d32e7e3df896c0bf285dbb548102f51502b7
Nathan Oliveira <125297833+Raonhn@users.noreply.github.com>
2023-11-29 13:23:09 -0300

    Update ScriptShell.sh

--- ScriptShell.sh
+++ ScriptShell.sh
@@ -249,6 +249,12 @@ sleep 2
 echo "$(tput setaf 10)[Trackware assistant]:$(tput setaf 7) Por fim faremos a clonagem do JAR da aplicação.."
  echo "$(tput setaf 10)[Trackware assistant]:$(tput setaf 7) Clonando o projeto do GitHub"
     git clone https://github.com/Sprint04/JAR-Grupo.git
+    cd JAR-Grupo
+    APItrackware.jar mv /~
+    rm Readme.md
+    rm Processos.jar
+    cd ..
+    rmdir JAR-Grupo
     if [ $? -eq 0 ]; then
         echo "$(tput setaf 10)[Trackware assistant]:$(tput setaf 7) Projeto clonado com sucesso."
     else
